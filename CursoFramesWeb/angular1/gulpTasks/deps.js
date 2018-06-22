ec4bc2bd2f86a57977c0bccb0cf11b34f46c6dbb
Jordan Rocha <70rd23@gmail.com>
2018-06-22 17:48:08 -0300

    Implementação dos tasks de minificação dos assets

--- CursoFramesWeb/angular1/gulpTasks/deps.js
+++ CursoFramesWeb/angular1/gulpTasks/deps.js
@@ -1,15 +1,43 @@
 const gulp = require('gulp')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
-gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
+gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts']) // Ao iniciar chama todas as outras tasks dentro do array
 
 gulp.task('deps.js', () => {
-  
+  return gulp.src([
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+    'node_modules/angular-animate/angular-animate.min.js',
+    'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
+    'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+    'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
+    'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
+    'node_modules/admin-lte/dist/js/app.min.js'
+  ])
+  .pipe(uglify()) // Executa um "desembelezamento" nos arquivos .js para reduzir o tamanho
+  .pipe(concat('deps.min.js')) // Concatena todos eles em um único arquivo
+  .pipe(gulp.dest('public/assets/js')) // Salva esse único arquivo dentro da pasta especificada
 })
 
 gulp.task('deps.css', () => {
-
+  return gulp.src([
+    'node_modules/angular-toastr/dist/angular-toastr.min.css',
+    'node_modules/font-awesome/css/font-awesome.min.css',
+    'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
+    'node_modules/admin-lte/dist/css/AdminLTE.min.css',
+    'node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
+  ])
+  .pipe(uglifycss({"uglyComments": true}))
+  .pipe(concat('deps.min.css'))
+  .pipe(gulp.dest('public/assets/css'))
 })
 
 gulp.task('deps.fonts', () => {
-
+  return gulp.src([
+    'node_modules/font-awesome/fonts/*.*',
+    'node_modules/admin-lte/bootstrap/fonts/*.*'
+  ])
+  .pipe(gulp.dest('public/assets/fonts'))
 })
