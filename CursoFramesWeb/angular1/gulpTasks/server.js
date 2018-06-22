28dfa1448a9c05166b848e3b1959fd395ba85f8c
Jordan Rocha <70rd23@gmail.com>
2018-06-22 18:10:43 -0300

    Implementação do sistema de reload automático

--- CursoFramesWeb/angular1/gulpTasks/server.js
+++ CursoFramesWeb/angular1/gulpTasks/server.js
@@ -1,9 +1,18 @@
 const gulp = require('gulp')
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
 
 gulp.task('watch', () => {
-
+  watch('app/**/*.html', () => gulp.start('app.html')) // Adiciona um observador para analisar se houve alguma alteração
+  watch('app/**/*.css', () => gulp.start('app.css'))   // em qualquer arquivo e em qualquer pasta, das especificadas
+  watch('app/**/*.js', () => gulp.start('app.js'))
+  watch('assets/**/*.*', () => gulp.start('app.assets'))
 })
 
 gulp.task('server', ['watch'], () => {
-  
+  return gulp.src('public').pipe(webserver({ // Chama o webserver caso hajam alterações analisadas pelo Watcher
+    livereload: true,
+    port: 3000,
+    open: true // Manda abrir o browser
+  }))
 })
