28a4800ed3cfb2dd73bd999392616c94d2572b62
Jordan Rocha <70rd23@gmail.com>
2018-06-26 23:01:20 -0300

    Inicialização do Controller do Ciclo de Pagamentos

--- CursoFramesWeb/angular1/app/common/components/field.js
+++ CursoFramesWeb/angular1/app/common/components/field.js
@@ -6,6 +6,7 @@
       grid: '@',
       placeholder: '@',
       type: '@',
+      model: '=',
     },
     controller: [
       'gridSystem',
@@ -18,7 +19,7 @@
       <div class="form-group">
         <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
         <input class="form-control" id="{{$ctrl.id}}" placeholder="{{ $ctrl.placeholder }}"
-          type="{{ $ctrl.type }}">
+          type="{{ $ctrl.type }}" ng-model="$ctrl.model">
       </div>
     </div>
     `
